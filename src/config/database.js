5e320bbe63c2a7162690acbf679dc55be3fd1ad1
ProfL3501 <ProfL3501@lab.aca>
2023-07-11 20:49:25 -0500

    initial commit

---
+++ src/config/database.js
@@ -0,0 +1,27 @@
+import Sequelize from 'sequelize'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const hostname = process.env.HOSTNAME || '127.0.0.1'
+const username = process.env.USERNAME || 'postgres'
+const password = process.env.PASSWORD || '123'
+const database = process.env.DATABASE || 'cursos'
+const port = process.env.DBPORT || 5432
+const dialect = process.env.DIALECT || 'postgres'
+
+const sequelize = new Sequelize(database, username, password, {
+    host: hostname,
+    port,
+    dialect: dialect,
+    operatorAliases: false,
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 20000,
+        idle: 5000
+    }
+})
+
+
+export default sequelize
